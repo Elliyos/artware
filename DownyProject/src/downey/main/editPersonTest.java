37bc88f4c1966859d40ae01557ba6a23c60e32d6
tmay1 <TylerMay@10.100.4.202>
2016-10-29 14:31:02 -0500

    Added null parameter check to JUnit and Changed export file names

--- DownyProject/src/downey/main/editPersonTest.java
+++ DownyProject/src/downey/main/editPersonTest.java
@@ -46,4 +46,19 @@ public class editPersonTest {
 		
 		assertEquals(person2.getBio(), person1.getBio()  );
 	}
+	
+	/**
+	 * Checks to make sure the edit connection works when a null parameter is passed into an edit connection.
+	 * 
+	 * GUI makes sure you can not enter a null String for any of the parameters Name, Culture, Occupation, Gender or Bio
+	 */
+	@Test
+	public void testEditPersonNullParamater(){
+		Person person1 = new Person("Joe", "American", "Artist", "Male", "28 years old");
+		Person person2 = new Person(" ", "American", "Artist", "Male", " ");
+		person1.editPerson(" ", "American", "Artist", "Male", " ");
+		
+		assertEquals(person2.getName(), person1.getName());
+		assertEquals(person2.getBio(), person1.getBio());
+	}
 }
